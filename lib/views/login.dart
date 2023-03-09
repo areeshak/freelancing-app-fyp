1d6500edeadc78146758e007c9e9c38d8d776b6b
areeshak <98297607+areeshak@users.noreply.github.com>
2023-03-09 15:00:27 +0500

    Add email and google auth

---
+++ lib/views/login.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:freelancing_fyp/services/firebase_auth_methods.dart';
+import 'package:freelancing_fyp/views/signup_email_password_screen.dart';
+
+import 'login_email_password_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => EmailPasswordSignup(),
+                  ),
+                );
+              },
+              child: const Text('Email Sign-up'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => EmailPasswordLogin(),
+                  ),
+                );
+              },
+              child: const Text('Email Login'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                FirebaseAuthMethods(FirebaseAuth.instance)
+                    .signInWithGoogle(context);
+              },
+              child: const Text('Google Sign-up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
