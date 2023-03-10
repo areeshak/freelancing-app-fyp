970c31f3779c5ce15790874dd6d1876c6366df75
areeshak <98297607+areeshak@users.noreply.github.com>
2023-03-11 01:45:45 +0500

    Display user details

--- lib/views/userProfile.dart
+++ lib/views/userProfile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freelancing_fyp/services/firebase_auth_methods.dart';
+import 'package:freelancing_fyp/widgets/custom_button.dart';
 import 'package:provider/provider.dart';
 
 class UserProfileScreen extends StatelessWidget {
@@ -8,37 +9,96 @@ class UserProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = context.read<FirebaseAuthMethods>().user; //access user
+    final check = user.providerData.first.providerId;
 
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        if (!user.emailVerified)
-          OutlinedButton(
-              onPressed: () {
-                context
-                    .read<FirebaseAuthMethods>()
-                    .sendEmailVerification(context);
-              },
-              child: const Text('verify email'),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 40,
           ),
-        OutlinedButton(
-            onPressed: () {
-              context
-                  .read<FirebaseAuthMethods>()
-                  .signOut(context);
-            },
-            child: const Text('sign out'),
-        ),
-        OutlinedButton(
-          onPressed: () {
-            context
-                .read<FirebaseAuthMethods>()
-                .deleteAccount(context);
-          },
-          child: const Text('delete account'),
-        )
-      ],
-    ));
+          Container(
+            padding: const EdgeInsets.all(18.0),
+            child: const Text(
+              'User Profile',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 30,
+              ),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: check == "google.com"
+                    ? NetworkImage(user.photoURL!)
+                    : const NetworkImage(
+                        'https://cdn-icons-png.flaticon.com/512/1144/1144709.png'),
+              ),
+              const SizedBox(
+                width: 40,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        user.displayName != null
+                            ? user.displayName!
+                            : "namemissing@emailuser.com",
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                    Text(
+                      user.email!,
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CustomButton(
+                onTap: () {
+                  context.read<FirebaseAuthMethods>().signOut(context);
+                },
+                text: 'Sign out',
+                sizeFactor: 0.4,
+              ),
+              // SizedBox(width: 20,),
+              CustomButton(
+                onTap: () {
+                  context.read<FirebaseAuthMethods>().deleteAccount(context);
+                },
+                text: 'Delete Account',
+                sizeFactor: 0.4,
+                buttonColor: Colors.red,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
